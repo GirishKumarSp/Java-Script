1f3bb155a5a3f7e60fee5f50a605360c263ac966
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2022-07-12 00:34:23 +0530

    Create Print Hello World.js

---
+++ Basics/Print Hello World.js
@@ -0,0 +1,14 @@
+// We will use these three ways to print 'Hello, World!'.
+
+// console.log()
+// alert()
+// document.write()
+
+// the hello world program
+console.log('Hello World');
+
+// the hello world program
+alert("Hello, World!");
+
+// the hello world program
+document.write('Hello, World!');
