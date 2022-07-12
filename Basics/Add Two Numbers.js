13897a4722a8d4c66498a0adaeb95b2ef5862baf
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2022-07-12 21:06:29 +0530

    Create Add Two Numbers.js

---
+++ Basics/Add Two Numbers.js
@@ -0,0 +1,24 @@
+//1
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+//2
+//Add Two Numbers Entered by the User
+//prompt() is used to take inputs from the user. parseInt() is used to convert the user input string to number.
+
+const prompt = require("prompt-sync")({ sigint: true});
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
