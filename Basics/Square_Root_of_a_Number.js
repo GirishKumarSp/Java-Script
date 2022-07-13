6f630f01076f2f47adeeee1dd785e81c09951473
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2022-07-13 13:52:16 +0530

    Update Square_Root_of_a_Number.js

--- Basics/Square_Root_of_a_Number.js
+++ Basics/Square_Root_of_a_Number.js
@@ -3,3 +3,8 @@ const number = prompt('Enter the number: ');
 
 const result = Math.sqrt(number);
 console.log(`The square root of ${number} is ${result}`);
+
+// Output
+
+// Enter the number: 9 
+// The square root of 9 is 3
